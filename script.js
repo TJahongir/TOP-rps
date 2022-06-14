0dd5eccdb1c4c9cf7b71ed6b96367217b3cac9b3
TJahongir <jakhongirt@gmail.com>
2022-06-14 02:34:14 -0400

    DOM access

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let playerSelection = 'rock';
-//let playerName = prompt("What is your name?" , 'Player')
+const roundCounter = document.querySelector('.round')
+const playerName = document.querySelector('.player_name')
+const playerScore = document.querySelector('.score_player') 
+const computerScore = document.querySelector('.score_computer') 
+const roundResultText = document.querySelector('.round-result')
+const cards = [...document.querySelectorAll('.cards')]
+
+
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+
+cards.map(card => {
+    card.addEventListener('click', (e) => playerSelection = e.target.name)
+})
+
+
 
 let computerPlay = (randomNumber) => {
     if (randomNumber === 0) {
@@ -13,21 +27,31 @@ let computerPlay = (randomNumber) => {
 
 let playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
-        return `${playerSelection} and ${computerSelection} is always a Tie!`
+        roundResultText.textContent = `${playerSelection} and ${computerSelection} is always a Tie!`
     } else if(playerSelection === 'rock' && computerSelection === 'scissors' ||
                 playerSelection ==='scissors' && computerSelection === 'paper' ||
                 playerSelection === 'paper' && computerSelection === 'rock') {
-        return `You Won! ${playerSelection} beats ${computerSelection}!`
+                playerScoreCount++
+                playerScore.textContent = playerScoreCount
+                roundResultText.textContent = `You Won! ${playerSelection} beats ${computerSelection}`
+
     } else {
-        return `You Lost! ${computerSelection} beats ${playerSelection}!`
+        computerScoreCount++
+        computerScore.textContent = computerScoreCount + 1
+        roundResultText.textContent = `You Lost! ${computerSelection} beats ${playerSelection}!`
     }
 
 
 }
 
 let game = () => {
-    for(let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerPlay(randomNumberPicker())));
+    //playerName.textContent = prompt("What is your name?" , 'Player')
+    let playerSelection = 'paper';
+    computerScore.textContent = computerScoreCount
+    playerScore.textContent = playerScoreCount
+    playRound(playerSelection, computerPlay(randomNumberPicker()))
+    for(let i = 0; i < 5; ++i) {
+        roundCounter.textContent = `Round ${i + 1}`
     }
 }
 
