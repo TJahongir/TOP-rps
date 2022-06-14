5f20425559506a8fcd03dda9774bab610f47dc3a
TJahongir <jakhongirt@gmail.com>
2022-06-14 13:52:59 -0400

    finished script part

--- script.js
+++ script.js
@@ -8,21 +8,30 @@ const cards = [...document.querySelectorAll('.cards')]
 
 // Modal
 const modal = document.querySelector('.modal')
-const modalBtn = document.querySelector('.modal_button')
-const modalTitle = document.querySelector('.modal_title')
+const modalBtn = document.querySelector('.modal-button')
+const modalTitle = document.querySelector('.modal-title')
+const darken = document.querySelector('.darken')
+
 
 
 let playerScoreCount = 0;
 let computerScoreCount = 0;
-
-cards.map(card => {
-    card.addEventListener('click', (e) => playerSelection = e.target.name)
-})
-
+let round = 1
 
 // Helper Functions
 let randomNumberPicker = () => (Math.floor(Math.random() * 3))
 
+let firstCharUpperCase = (string) =>{
+    string = string.charAt(0).toUpperCase() + string.slice(1)
+    return string
+}
+
+let startOver = (e) => {
+    darken.style.visibility = 'hidden'
+    roundResultText.textContent = "Let's Start Again!"
+}
+
+modalBtn.addEventListener('click', (e) => startOver(e))
 
 let computerPlay = (randomNumber) => {
     if (randomNumber === 0) {
@@ -34,40 +43,53 @@ let computerPlay = (randomNumber) => {
 }
 }
 
-let playRound = (playerSelection, computerSelection) => {
-    // while (playerScoreCount !== 5 || computerScoreCount !== 5) {
+let restartScores = () =>  {
+    playerScoreCount = 0
+    computerScoreCount = 0
+    round = 1
+    playerScore.textContent = playerScoreCount;
+    computerScore.textContent = computerScoreCount;
+    roundCounter.textContent = `Round ${round}`
+}
 
-    // }
+let modalActive = () => {
+    modalTitle.textContent = playerScoreCount === 5 ? 'You Won!' :
+                             computerScoreCount === 5 ? 'You Lost! Wanna Try Again?':
+                             'Wassup'
+    restartScores()
+    darken.style.visibility = 'visible'
+}
+
+let playerPlay = (e) => {
+    playerSelection = e.target.name;
+    let computerSelection = computerPlay(randomNumberPicker())
+    playRound(playerSelection, computerSelection);
+    playerScoreCount === 5 || computerScoreCount === 5 ? modalActive() : 'not yet'
+}
+
+cards.map(card => {
+    card.addEventListener('click', playerPlay)
+})
+
+let playRound = (playerSelection, computerSelection) => {
+    computerScore.textContent = computerScoreCount
+    playerScore.textContent = playerScoreCount
+    roundCounter.textContent = `Round ${++round}`
+    
     if (playerSelection === computerSelection) {
-        roundResultText.textContent = `${(playerSelection).toUpperCase()} and ${computerSelection} is always a Tie!`
+        roundResultText.textContent = `It is a Tie!`
     } else if(playerSelection === 'rock' && computerSelection === 'scissors' ||
                 playerSelection ==='scissors' && computerSelection === 'paper' ||
                 playerSelection === 'paper' && computerSelection === 'rock') {
                 playerScoreCount++
                 playerScore.textContent = playerScoreCount
-                roundResultText.textContent = `You Won! ${playerSelection} beats ${computerSelection}`
+                roundResultText.textContent = `You Won! ${firstCharUpperCase(playerSelection)} > ${computerSelection}`
 
     } else {
         computerScoreCount++
-        computerScore.textContent = computerScoreCount + 1
-        roundResultText.textContent = `You Lost! ${computerSelection} beats ${playerSelection}!`
+        computerScore.textContent = computerScoreCount
+        roundResultText.textContent = `You Lost! ${computerSelection} > ${playerSelection}!`
     }
 
 
 }
-
-let game = () => {
-    //playerName.textContent = prompt("What is your name?" , 'Player')
-    computerScore.textContent = computerScoreCount
-    playerScore.textContent = playerScoreCount
-    playerSelection = 'rock'
-    playRound(playerSelection, computerPlay(randomNumberPicker()))
-    for(let i = 0; i < 5; ++i) {
-        console.log('Here!' + i)
-        roundCounter.textContent = `Round ${i + 1}`
-    }
-}
-
-
-
-game();
